6b0955f7e379d451eb1b744974922ba7ed42574c
Lars <lars.leske@web.de>
2018-11-20 23:19:40 +0100

    erste Session noch keien Anbindung an hearstone API, bisher nur Ionic

---
+++ src/app/card/card.module.ts
@@ -0,0 +1,21 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CardDeckPage } from './card-deck/card-deck.page';
+
+import {CardService} from './shared/card.service';
+
+@NgModule({
+    imports: [
+        IonicModule,
+        CommonModule
+    ],
+    providers: [
+        CardService
+    ],
+    declarations: [
+        CardDeckPage
+    ]
+})
+export class CardPageModule {}
